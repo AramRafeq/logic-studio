10251da383191f8a1fe9a67e9816bc1475377294
Aram Rafeq <aramrafeq2@gmail.com>
2022-12-27 22:58:29 +0300

    reactflow setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import './App.css';
 
-import React, { ReactElement, useState } from 'react';
+import React, { useCallback } from 'react';
+import ReactFlow, { Background, useNodesState, useEdgesState, addEdge } from 'reactflow';
+import 'reactflow/dist/style.css';
 
-function App(): ReactElement {
-  return <h1>hello world </h1>;
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+export default function Flow(): React.ReactElement {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+      }}
+    >
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Background />
+      </ReactFlow>
+    </div>
+  );
 }
-
-export default App;
